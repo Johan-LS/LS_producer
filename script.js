bfdef03a9c23213bc6ccdfb578d62de4b0cdf7ca
Johan <johanquispesalas7777@gmail.com>
2025-03-21 17:16:34 -0500

    initial commit

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById('btn-proyectos').addEventListener('click', function () {
+    const carrusel = document.getElementById('carrusel');
+    carrusel.classList.toggle('oculto');
+});
